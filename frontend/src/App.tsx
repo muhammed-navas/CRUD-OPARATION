7d3343445e29e3383581e1fcf6bc88b6806e102a
muhammed navas <muhammednavas6287@gmail.com>
2025-01-27 23:47:55 +0530

    added new file like api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-import SimpleTodoApp from "./page/SimpleTodoApp"
-import './App.css'
-
+// import SimpleTodoApp from "./page/SimpleTodoApp"
+// import "./App.css";
+import { ApiData } from "./components/ApiData.js";
 
 const App = () => {
   return (
     <div className="main">
       {/* <SimpleTodoApp /> */}
+      <ApiData />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
